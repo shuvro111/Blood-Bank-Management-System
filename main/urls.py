707499c981f7ee4f1d5e1f61dc1362c6178c133f
Back-Spac3 <backspacecrew18@gmail.com>
2021-02-03 01:35:46 +0600

    Added Routes To Homepage, Create Account and Login Page

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('create/', views.create__account, name = 'create__account'),
+    path('login/', views.log__in, name = 'log__in'),
     path('admin/', admin.site.urls),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+def create__account(request):
+    return render(request, 'main/create__account.html') 
+ 
+def log__in(request):
+    return render(request, 'main/log__in.html') 
+
